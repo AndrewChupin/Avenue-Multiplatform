15cfcec985118271858989fcfa9b3c7f897fdec4
Andrew Chupin <artas.96@mail.ru>
2018-11-30 23:04:52 +0400

    Переименован базовый модуль iOS приложения

--- android_app/src/main/kotlin/com/avenue/client/presentation/main/MainActivity.kt
+++ android_app/src/main/kotlin/com/avenue/client/presentation/main/MainActivity.kt
@@ -3,7 +3,6 @@ package com.avenue.client.presentation.main
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.avenue.client.R
-import com.avenue.client.presentation.trips.TripsFragment
 
 class MainActivity : AppCompatActivity() {
 
